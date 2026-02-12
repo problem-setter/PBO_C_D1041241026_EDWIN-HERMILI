b0725ef0f1fb2872dee5313800d56c905dd03373
problem-setter <d1041241026@student.untan.ac.id>
2026-02-12 16:56:50 +0700

    First Commit

---
+++ learning.java
@@ -0,0 +1,22 @@
+/*
+NIM     : D1041241026
+Nama    : Edwin Hermili
+Program untuk variable
+*/
+
+
+public class learning {
+    public static void main(String[] args) {
+        // Dekrasi dan inisialisasi variabel
+        int angka = 10;
+
+        // Menampilkan nilai awal variabel
+        System.out.println("Nilai awal angka: " + angka);
+        
+        // Mengubah nilai variabel
+        angka = 25;
+
+        // Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
