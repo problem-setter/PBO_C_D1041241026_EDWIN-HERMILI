cf814ad0eda3634c0b2af43c058ac19c9412a2eb
problem-setter <d1041241026@student.untan.ac.id>
2026-02-13 22:21:14 +0700

    Updated

---
+++ Tugas PBO/LatihLiteral.java
@@ -0,0 +1,22 @@
+public class LatihLiteral {
+    public static void main(String[] args) {
+        byte nilaiMaksimumByte = 127;
+        short tahunSaatIni = 2026;
+        int populasiIndonesia = 275000000;
+        long populasiDunia = 8000000000L;
+        float pi = 3.14F;
+        double e = 2.718281828;
+        char hurufPertama = 'E';
+        boolean status = true;
+
+        System.out.println("=== DEMONSTRASI LITERAL TIPE DATA ===");
+        System.out.println("Tipe byte           : " + nilaiMaksimumByte);
+        System.out.println("Tipe short          : " + tahunSaatIni);
+        System.out.println("Tipe int            : " + populasiIndonesia);
+        System.out.println("Tipe long           : " + populasiDunia);
+        System.out.println("Tipe float          : " + pi);
+        System.out.println("Tipe double         : " + e);
+        System.out.println("Tipe hurufPertama   : " + hurufPertama);
+        System.out.println("Tipe status         : " + status);
+    }
+}
---
+++ Tugas PBO/LatihPerhitungan.java
@@ -0,0 +1,15 @@
+public class LatihPerhitungan {
+    public static void main(String[] args) {
+        int saldo = 1000000;
+        System.out.println("Saldo Awal               : " + saldo);
+        int setoran = 500000;
+        System.out.println("Setoran                  : " + setoran);
+        saldo += setoran;
+        System.out.println("Saldo Setelah Setor      : " + saldo);
+
+        int penarikan = 250000;
+        System.out.println("Penarikan                : " + penarikan);
+        saldo -= penarikan;
+        System.out.println("Saldo Akhir              : " + saldo);
+    }
+}
