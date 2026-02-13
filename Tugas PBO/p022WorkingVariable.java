b427713bc3b8a619f09dc3634f41bc76a5b12f03
problem-setter <d1041241026@student.untan.ac.id>
2026-02-13 21:56:40 +0700

    First Commit

---
+++ Tugas PBO/p022WorkingVariable.java
@@ -0,0 +1,21 @@
+/*
+NIM     : D1041241026
+Nama    : Edwin Hermili
+Program untuk variable
+*/
+
+public class p022WorkingVariable {
+    public static void main(String[] args) {
+           // Deklarasi dan inisialisasi variabel
+           int angka = 10;
+           
+           // Menampilkan nilai awal variabel
+           System.out.println("Nilai awal angka: " + angka);
+
+           // Mengubah nilai variabel
+           angka = 25;
+
+           // Menampilkan nilai setelah perubahan
+           System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
