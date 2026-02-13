95e5aa8d35abbdc83b6b341237957da168a634fe
problem-setter <d1041241026@student.untan.ac.id>
2026-02-13 22:08:52 +0700

    Updated

---
+++ Tugas PBO/LatihDataMahasiswa.java
@@ -0,0 +1,18 @@
+public class LatihDataMahasiswa {
+    public static void main(String[] args) {
+        String nim = "230101001";
+        String nama = "Budi Santoso";
+        char kelas = 'A';
+        boolean statusAktif = true;
+        double ipk = 3.75f;
+        int totalSks = 72;
+
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM               : " + nim);
+        System.out.println("Nama              : " + nama);
+        System.out.println("Kelas             : " + kelas);
+        System.out.println("Status Aktif      : " + statusAktif);
+        System.out.println("IPK Semester      : " + ipk);
+        System.out.println("Total SKS         : " + totalSks);
+    }
+}
